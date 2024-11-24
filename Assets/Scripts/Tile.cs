230dc6a26c642035d93813366040008acf2b4e2d
tnarwhal <nate.rawnsley@gmail.com>
2024-11-24 14:11:43 +0000

    Added randomly assigned decorations

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,4 +5,24 @@ public class Tile : MonoBehaviour {
     public TileTerrain terrainType;
     public List<Tile> adjacentTiles = new List<Tile>();
     public Unit unitHere;
+    private GameObject decoration;
+
+    public void SetTerrain() {
+        if (decoration != null) {
+            Destroy(decoration);
+            decoration = null;
+        }
+        GetComponent<Renderer>().material = terrainType.material;
+        if (terrainType.decorations.Count > 0) {
+            if (Random.value <= terrainType.decorationFrequency) {
+                int index = Random.Range(0, terrainType.decorations.Count);
+                decoration = Instantiate(terrainType.decorations[index], transform);
+
+                int alignment = Random.Range(0, 4);
+                Vector3 decoRotation = decoration.transform.rotation.eulerAngles;
+                decoRotation.y = alignment * 90;
+                decoration.transform.rotation = Quaternion.Euler(decoRotation);
+            }
+        }
+    }
 }
